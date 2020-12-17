8f8182b4098995862e3a31ac1b142878a156bf73
Jonathan-Majors <izzymajors@gmail.com>
2020-12-17 13:17:41 -0600

    lunch break

--- index.js
+++ index.js
@@ -118,8 +118,19 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian{
+constructor(obj){
+  super(obj);
+  this.specialty = obj.specialty;
+  this.favLanguage = obj.favLanguage;
+  this.catchPhrase = Obj.catchPhrase
+}
+demo(){
 
+}
+grade(){
+
+}
  }
   /*
     TASK 5
@@ -136,9 +147,10 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
-     
- }
+ class Student extends Lambdasian{
+    
+
+     }
   
   /*
     TASK 6
@@ -153,8 +165,10 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor{
+     constructor(){
+       
+     }
  }
   /*
     STRETCH PROBLEM (no tests!)
